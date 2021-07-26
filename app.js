411c44a6a70140734a8ea0d68ab20aa853ef2db1
Francisco Iván López Quihuis <72483051+FcoLopezQ@users.noreply.github.com>
2021-07-26 18:07:05 -0500

    Upload 2do Parcial

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ## Agrega la dependencia de express ##
-
+var express = require("express");
 
 var animals = [
    {
@@ -26,13 +26,15 @@ var animals = [
 
 
 // ## Inicializa express ##
-
+var app = express();
+var PORT = 5000;
 
 // ## Inicializa el motor de plantillas con EJS ##
-
+app.set('view engine', 'ejs');
 
 // ## Agrega el middleware de express para que el servidor soporte json ##
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 /* ############## RUTAS ################  */
 
@@ -42,7 +44,9 @@ var animals = [
       a) title:  con el valor "All" 
       b) animals: con referencia al arreglo animals. 
 */
-
+app.get("/all-pets", function(req, res) {
+  res.render("pages/all-pets", {title: "All", animals: animals});
+});
 
 /* (2)  Crea una ruta POST que: 
    - escuche en /api/addAnimal 
@@ -50,14 +54,26 @@ var animals = [
    - lo agregue al arreglo animals
 
 */
- 
+app.post("/api/addAnimal", function(req, res) {
+  
+  var newAnimal = req.body;
+
+  console.log(newAnimal);
+
+  animals.push(newAnimal);
+
+  res.json(newAnimal);
+});
+
 /* (3)  Crea una ruta GET que: 
    - escuche en /dog  
    - renderice la página 'pages/dog' y reciba 1 objeto con 2 propiedades: 
       a) title:  con el valor "Dog" 
       b) animals: con el valor del indice[0]
 */ 
-
+app.get("/dog", function(req, res) {
+  res.render("pages/dog", {title: "Dog", dog: animals[0]});
+});
 
 /* (4)  Crea una ruta GET que: 
    - escuche en /api/getAnimal/:animal
@@ -68,9 +84,26 @@ var animals = [
       a) title:  con el valor obtenido de la ruta dinámica
       b) animal: con la variable que almacena el objeto encontrado. Si no lo encuentra la variable se va vacía
 */ 
-   
+app.get("/api/getAnimal/:animal", function(req, res) {
+  var chosen = req.params.animal;
 
+  console.log(chosen);
+
+  for (var i = 0; i < animals.length; i++) {
+    if (chosen === animals[i].animalType) {
+      var chosenAnimal = animals[i];
+      res.render("pages/any", {title: chosen, animal: chosenAnimal});
+      return res.json(characters[i]);
+    }
+  }
+
+  res.render("pages/any", {title: chosen, animal: null});
+
+  
+});
 
 //  Agrega el código necesario para que el servidor escuche en el puerto 5000
-
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
 
